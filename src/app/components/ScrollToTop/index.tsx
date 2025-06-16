f23e380a2cba3cae5f06815739004a5a7e1e0dc3
Wilson635 <87822542+Wilson635@users.noreply.github.com>
2025-06-16 16:41:30 +0100

    updated link button

--- src/app/components/ScrollToTop/index.tsx
+++ src/app/components/ScrollToTop/index.tsx
@@ -34,7 +34,7 @@ export default function ScrollToTop() {
       <div className='flex items-center gap-2.5'>
         <Link
           href={
-            'https://getnextjstemplates.com/products/chefs-kitchen-free-nextjs-landing-page-template'
+            'https://github.com/Wilson635/chef-kitchen-nextjs.git'
           }
           target='_blank'
           className='hidden lg:block bg-primary text-white hover:bg-darkmode text-sm px-4 py-3.5 leading-none rounded-lg font-medium text-nowrap'>
